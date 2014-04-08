9d2e139017484f54b8580369b287d092925ff033
Andrew <aj.cumine@btinternet.com>
2014-04-08 13:18:08 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YelpClone::Application.config.secret_key_base = '700cd325ad6740d8b3a906d290185929083cfeab05810ea708b25601be54ba0af6b503659910d63146235e4e43516bc3ce5050c8aa111436f687facc6a34e6b8'
