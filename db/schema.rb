115f6b7885d14061d7e3ba51d96eba6bdcf08a21
Andrew <aj.cumine@btinternet.com>
2014-04-09 14:03:50 +0100

    added review

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140408145332) do
+ActiveRecord::Schema.define(version: 20140408191725) do
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
     t.string   "category"
     t.string   "location"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.integer  "restaurant_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
---
+++ spec/features/reviews_feature_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'writing reviews'  do
+  
+  before(:each) do
+    Restaurant.create(name: 'Yo Sushi')
+  end
+
+  it 'should change the average score of the restaurant' do
+    visit '/restaurants'
+    click_link 'Review Yo Sushi'
+    fill_in 'Comment', with: 'omnomnom'
+    select '5', from: 'Rating'
+    click_button 'Create Review'
+    expect(page).to have_css '.average_review', text: '5'
+  end
+end
